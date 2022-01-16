cdf7deb6788a0e96a67632b34aa11336ddd8258c
sklee2685 <sklee2685@gmail.com>
2022-01-16 23:53:53 +0900

    html

---
+++ html/phpjs/1.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+  <html>
+      <head>
+          <meta charset="utf-8">
+      </head>
+      <body>
+          <h1>PHP</h1>
+          <?php
+          echo "Hello php";
+          echo 10+10;
+          ?>
+
+          <h1>JavaScript</h1>
+          <script>
+              document.write("Hello java");
+          </script>
+      </body>
+  </html>
